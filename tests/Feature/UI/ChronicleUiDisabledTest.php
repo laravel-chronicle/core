ffa2be56cc30f956042fe22103d27b8c8e4e8a1d
Vasileios Ntoufoudis <info@ntoufoudis.com>
2026-05-25 10:29:52 +0300

    feat: add ChronicleUiEnabled middleware with disabled tests

---
+++ tests/Feature/UI/ChronicleUiDisabledTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Chronicle\Http\Middleware\ChronicleUiEnabled;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+it('ChronicleUiEnabled aborts with 404 when disabled', function () {
+    config(['chronicle.ui.enabled' => false]);
+
+    $middleware = new ChronicleUiEnabled;
+    $request = Request::create('/chronicle');
+
+    expect(fn () => $middleware->handle($request, fn () => response('ok')))
+        ->toThrow(HttpException::class);
+});
+
+it('ChronicleUiEnabled passes through when enabled', function () {
+    config(['chronicle.ui.enabled' => true]);
+
+    $middleware = new ChronicleUiEnabled;
+    $request = Request::create('/chronicle');
+
+    $response = $middleware->handle($request, fn () => response('ok'));
+    expect($response->getContent())->toBe('ok');
+});
