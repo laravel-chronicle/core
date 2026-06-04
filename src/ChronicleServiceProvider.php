549631cdd94dcc8b7ef5decee0b3cae81513d7f8
Vasileios Ntoufoudis <info@ntoufoudis.com>
2026-06-04 17:45:33 +0300

    feat(commands): add chronicle:key:generate for Ed25519 keypair minting

--- src/ChronicleServiceProvider.php
+++ src/ChronicleServiceProvider.php
@@ -5,6 +5,7 @@ namespace Chronicle;
 use Chronicle\Console\Commands\CreateCheckpointCommand;
 use Chronicle\Console\Commands\ExportCommand;
 use Chronicle\Console\Commands\InstallCommand;
+use Chronicle\Console\Commands\KeyGenerateCommand;
 use Chronicle\Console\Commands\PruneCommand;
 use Chronicle\Console\Commands\ReportCommand;
 use Chronicle\Console\Commands\ShowEntryCommand;
@@ -90,6 +91,7 @@ class ChronicleServiceProvider extends ServiceProvider
                 StatsCommand::class,
                 ShowEntryCommand::class,
                 PruneCommand::class,
+                KeyGenerateCommand::class,
             ]);
         }
     }
